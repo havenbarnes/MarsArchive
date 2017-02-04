c33b9c6acbf30ea595ef50176aa48daa9625294b
Haven Barnes <haven38@gmail.com>
2017-02-03 20:34:43 -0600

    Initial Commit

---
+++ MarsArchive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarsArchive
+//
+//  Created by Haven Barnes on 2/3/17.
+//  Copyright © 2017 Azing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
